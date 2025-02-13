fd77a6d049a241307a4c6b84b1738db1bf5581eb
WRyan <wesley.ryan03@gmail.com>
2025-02-13 11:49:15 -0300

    Add files via upload

--- src/components/ItemList.jsx
+++ src/components/ItemList.jsx
@@ -1,23 +1,27 @@
 import React from "react";
 import SingleItem from "./SingleItem";
 
-const ItemList = ({ title, items }) => {
+const ItemList = ({ title, items, itemsArray, path, idPath }) => {
   // console.log(items)
 
   return (
     <div className="item-list">
       <div className="item-list__header">
         <h2>{title} populares</h2>
-        <a className="item-list__link" href="/">
+        <a href={path} className="item-list__link">
           Mostrar tudo
         </a>
       </div>
 
       <div className="item-list__container">
-        {Array(items)
-          .fill()
-          .map(() => (
-            <SingleItem />
+        {itemsArray
+          .filter((currentValue, index) => index < items)
+          .map((currObj, index) => ( 
+            <SingleItem 
+
+            idPath = {idPath}
+            {...currObj} 
+            key={`${title}-${index}`} />
           ))}
       </div>
     </div>
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,14 +1,29 @@
 import React from "react";
 import ItemList from "./ItemList";
+import { artistArray } from "../assets/database/artists";
+import { songsArray } from "../assets/database/songs";
 
 const Main = () => {
   return (
     <div className="main">
       {/* ITEM LIST DE ARTISTAS */}
-      <ItemList title="Artistas" items={10}/>
+      <ItemList
+        title="Artistas"
+        items={5}
+        itemsArray={artistArray}
+        path="/artists"
+        idPath="/artist"
+      />
 
       {/* ITEM LIST DE MÚSICAS */}
-      <ItemList title="Músicas" items={20}/>
+      <ItemList
+        title="Músicas"
+        items={10}
+        itemsArray={songsArray}
+        path="/songs"
+        idPath="/song"
+
+      />
     </div>
   );
 };

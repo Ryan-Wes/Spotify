a4a202a555e46f7c250417cd7ad08d8e2a26fe2b
WRyan <wesley.ryan03@gmail.com>
2025-02-14 15:05:51 -0300

    Add files via upload

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -3,27 +3,34 @@ import ItemList from "./ItemList";
 import { artistArray } from "../assets/database/artists";
 import { songsArray } from "../assets/database/songs";
 
-const Main = () => {
+const Main = ({ type }) => {
   return (
     <div className="main">
       {/* ITEM LIST DE ARTISTAS */}
-      <ItemList
-        title="Artistas"
-        items={5}
-        itemsArray={artistArray}
-        path="/artists"
-        idPath="/artist"
-      />
+      {type === "artists" || type === undefined? (
+        <ItemList
+          title="Artistas"
+          items={10}
+          itemsArray={artistArray}
+          path="/artists"
+          idPath="/artist"
+        />
+      ) : (
+        <></>
+      )}
 
       {/* ITEM LIST DE MÚSICAS */}
-      <ItemList
-        title="Músicas"
-        items={10}
-        itemsArray={songsArray}
-        path="/songs"
-        idPath="/song"
-
-      />
+      {type === "songs" || type === undefined? (
+        <ItemList
+          title="Músicas"
+          items={20}
+          itemsArray={songsArray}
+          path="/songs"
+          idPath="/song"
+        />
+      ) : (
+        <></>
+      )}
     </div>
   );
 };
